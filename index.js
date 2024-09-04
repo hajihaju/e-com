95c2dd4122e5bc7bcb9424c57b511c1a3735e465
hajihaju <hajihaju19@gmail.com>
2024-09-04 11:50:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const User=require("./User")
 const app=express();
 const connectDB=async()=>{
     mongoose.connect('mongodb://localhost:27017/e-commerce')
-    const usersSchema=new mongoose.Schema({});
-    const users=mongoose.model('users',usersSchema)
+    const UserSchema=new mongoose.Schema({});
+    const User=mongoose.model('User',UserSchema)
     const data=await users.find();
     console.warn(data);
 }
