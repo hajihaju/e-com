d72babd9bbd389c9b806c91be5ce1b4c01745d61
hajihaju <hajihaju19@gmail.com>
2024-09-04 11:55:01 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,11 +1,10 @@
 
 const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     name: String,
    email: String,
    password: String
 
 });
 
-module.exports = mongoose.model("user",userSchema);
+module.exports = mongoose.model("product",productSchema);
